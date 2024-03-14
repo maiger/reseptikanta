fba31238607d94174e0d0f12f669f47c243f5c3f
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2024-03-14 16:23:34 +0200

    Add simple content to About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Tervetuloa Reseptikantaan!</h1>
+    <p>Täältä löydät herkullisimmat reseptit jokaiseen tilanteeseen.</p>
+    <h2>Ominaisuudet</h2>
+    <ul>
+      <li>Avoin hakukenttä</li>
+      <li>Haku avainsanoilla</li>
+      <li>Reseptien vaiheitten seuranta</li>
+    </ul>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    /* min-height: 100vh; */
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
+    min-height: 80vh;
+    margin-top: 3rem;
+    color: var(--color-secondary-1-dark-1);
+
+    h2 {
+      margin: 3rem 0 0 0;
+    }
+
+    ul {
+      margin-top: 1rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
   }
 }
 </style>
