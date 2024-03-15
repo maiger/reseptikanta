b017ff15ae0d450a2479281d4e57b24d81410777
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2024-03-15 15:28:20 +0200

    Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/reseptikanta/",
+  base: "/reseptikanta/",
   plugins: [vue()],
   resolve: {
     alias: {
