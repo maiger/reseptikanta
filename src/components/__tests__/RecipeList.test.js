5a5c6e8f8754cec06007bc1978713b575a30b282
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2024-04-04 15:26:10 +0300

    Add basic tests for RecipeList

---
+++ src/components/__tests__/RecipeList.test.js
@@ -0,0 +1,78 @@
+import { render, screen } from "@testing-library/vue";
+import { describe, it, expect } from "vitest";
+import RecipeList from "../RecipeList.vue";
+
+describe.concurrent("RecipeList", () => {
+  // Setup recipe
+  const recipes = [
+    {
+      id: 1,
+      title: "Test Recipe Title",
+      description: "",
+      source: "Test Source",
+      prepTime: 60,
+      servings: 4,
+      tags: ["kala", "liha"],
+      img: "test_image.JPG",
+      dateAdded: "1.1.1970",
+      favourite: false,
+      instructions: ["Lorem ipsum dolor sit amet."],
+      ingredients: [
+        {
+          collectionName: "Kala",
+          collectionIngredients: [
+            {
+              name: "Ingredient_Lorem",
+              amount: 1.2,
+              unit: "unit_kg",
+            },
+            {
+              name: "Ingredient_Ipsum",
+              amount: 1,
+              unit: "tl",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Test 2 Recipe Title",
+      description: "",
+      source: "Test Source 2",
+      prepTime: 100,
+      servings: 6,
+      tags: ["kana", "keitto"],
+      img: "test_image_2.JPG",
+      dateAdded: "1.1.1970",
+      favourite: false,
+      instructions: ["Lorem ipsum dolor sit amet."],
+      ingredients: [
+        {
+          collectionName: "Kala",
+          collectionIngredients: [
+            {
+              name: "Ingredient_Lorem",
+              amount: 1.2,
+              unit: "unit_kg",
+            },
+            {
+              name: "Ingredient_Ipsum",
+              amount: 1,
+              unit: "tl",
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  it("Render recipeList correctly", async () => {
+    // Arrange
+    render(RecipeList, { props: { recipes } });
+
+    // Assert
+    expect(screen.getByText(recipes[0].title)).not.toBeNull();
+    expect(screen.getByText(recipes[1].title)).not.toBeNull();
+  });
+});
