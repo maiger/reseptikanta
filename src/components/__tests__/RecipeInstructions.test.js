123f351a92d4c711adbdf8c6aeccef399b138489
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2024-04-04 12:35:40 +0300

    Add basic test for RecipeInstructions

---
+++ src/components/__tests__/RecipeInstructions.test.js
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/vue";
+import { describe, it, expect } from "vitest";
+import RecipeInstructions from "../RecipeInstructions.vue";
+
+describe.concurrent("RecipeInstructions", () => {
+  // Setup recipe
+  const recipe = {
+    id: 1,
+    title: "Test Recipe Title",
+    description: "",
+    source: "Test Source",
+    prepTime: 60,
+    servings: 4,
+    tags: ["test_tag"],
+    img: "test_image.JPG",
+    dateAdded: "1.1.1970",
+    favourite: false,
+    instructions: ["Lorem ipsum dolor sit amet."],
+    ingredients: [
+      {
+        collectionName: "Kala",
+        collectionIngredients: [
+          {
+            name: "Lorem",
+            amount: 1.2,
+            unit: "kg",
+          },
+          {
+            name: "Ipsum",
+            amount: 1,
+            unit: "tl",
+          },
+        ],
+      },
+    ],
+  };
+
+  it("Render recipeInstructions correctly", async () => {
+    // Arrange
+    render(RecipeInstructions, { props: { recipe } });
+
+    // Assert
+    expect(screen.getByText("Ohjeet")).not.toBeNull();
+    expect(screen.getByText("Vaihe 1:")).not.toBeNull();
+    // Should be null as there is no "Vaihe 2:"
+    expect(screen.queryByText("Vaihe 2:")).toBeNull();
+  });
+});
