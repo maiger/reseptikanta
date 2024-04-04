8e300e42f704d615e21fac28987eaa9643c8d8d8
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2024-04-04 13:16:23 +0300

    Add basic tests for RecipeIngredients

---
+++ src/components/__tests__/RecipeIngredients.test.js
@@ -0,0 +1,75 @@
+import { render, screen } from "@testing-library/vue";
+import { describe, it, expect } from "vitest";
+import RecipeIngredients from "../RecipeIngredients.vue";
+
+describe.concurrent("RecipeIngredients", () => {
+  const servings = 4;
+
+  // Setup recipe
+  const recipe = {
+    id: 1,
+    title: "Test Recipe Title",
+    description: "",
+    source: "Test Source",
+    prepTime: 60,
+    servings: 4,
+    tags: ["test_tag"],
+    img: "test_image.JPG",
+    dateAdded: "1.1.1970",
+    favourite: false,
+    instructions: ["Lorem ipsum dolor sit amet."],
+    ingredients: [
+      {
+        collectionName: "Kala",
+        collectionIngredients: [
+          {
+            name: "Ingredient_Lorem",
+            amount: 1.2,
+            unit: "unit_kg",
+          },
+          {
+            name: "Ingredient_Ipsum",
+            amount: 1,
+            unit: "tl",
+          },
+        ],
+      },
+    ],
+  };
+
+  it("Render recipeIngredients correctly", async () => {
+    // Arrange
+    render(RecipeIngredients, { props: { recipe, servings } });
+
+    // Assert
+    expect(screen.getByText("Ainesosat")).not.toBeNull();
+    expect(
+      screen.getByText(recipe.ingredients[0].collectionIngredients[0].name)
+    ).not.toBeNull();
+    expect(screen.getByText("1 1/5 unit_kg")).not.toBeNull();
+    expect(
+      screen.getByText(recipe.ingredients[0].collectionIngredients[1].name)
+    ).not.toBeNull();
+  });
+
+  it("Render custom amounts correctly", async () => {
+    // Arrange
+    render(RecipeIngredients, {
+      props: { recipe, servings: 8 },
+    });
+
+    // Assert
+    expect(screen.getByText("2 2/5 unit_kg")).not.toBeNull();
+  });
+
+  it("Render negative amounts correctly", async () => {
+    // Amounts lower limit should be 1. No negatives allowed.
+    // Arrange
+    render(RecipeIngredients, {
+      props: { recipe, servings: -1 },
+    });
+
+    // Assert
+    expect(screen.getByText("3/10 unit_kg")).not.toBeNull();
+  });
+});
